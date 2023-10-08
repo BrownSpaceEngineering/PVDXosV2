4f3a0d4d19ef8be2d2333504059df31bee9d2b9f
Oren Kohavi <oren_kohavi@brown.edu>
2023-10-08 13:55:38 -0400

    Delete temp files and update gitignore

--- main.c
+++ main.c
@@ -1,13 +1,39 @@
 #include <atmel_start.h>
+#include <hal_adc_sync.h>
+#include <utils.h>
+#include <driver_init.h>
 
+uint8_t arr[100];
+/*
+ * ADC_0
+ * - imported from driver_init.h, an adc_sync_descriptor
+*/
+/*
+ * type Adc and basically all registers are defined in adc.h
+ */
 int main(void)
 {
-	/* Initializes MCU, drivers and middleware */
-	//atmel_start_init();
+    /* Initializes MCU, drivers and middleware */
+    //atmel_start_init();
 
-	/* Replace with your application code */
-    volatile int x = 0;
+    // Initialize ADC
+
+    adc_sync_init(&ADC_0, ADC0, NULL);
+
+	const uint8_t reading_size = 1;
+
+	adc_sync_enable_channel(&ADC_0,0);
+	int counter = 0;
+	adc_sync_set_resolution(&ADC_0, 8); //8 for 8-bit
+    /* Replace with your application code */
+
 	while (1) {
-        x++;
-	}
+		adc_sync_read_channel(&ADC_0,0, &arr[counter],reading_size);
+        // Collect ADC readings and store them
+		counter += 1;
+        delay_ms(500); //500ms
+    }
 }
+
+
+
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # Default configuration
 CND_ARTIFACT_DIR_Default=dist/Default/production
-CND_ARTIFACT_NAME_Default=PVDXosThirdTry.X.production.hex
-CND_ARTIFACT_PATH_Default=dist/Default/production/PVDXosThirdTry.X.production.hex
+CND_ARTIFACT_NAME_Default=PVDXosV2.production.hex
+CND_ARTIFACT_PATH_Default=dist/Default/production/PVDXosV2.production.hex
