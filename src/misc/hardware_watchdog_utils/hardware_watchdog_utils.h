93bc2477813129e08348c84f8e4a14f77e0d7f6a
Tanish Makadia <tanishmakadia@gmail.com>
2024-02-22 17:57:10 -0500

    style: Change position of curly brackets for hardware watchdog utils

--- src/misc/hardware_watchdog_utils/hardware_watchdog_utils.h
+++ src/misc/hardware_watchdog_utils/hardware_watchdog_utils.h
@@ -3,16 +3,15 @@
 
 #include "atmel_start.h"
 
-// This file defines copies of functions from (ASF/hri/hri_wdt_d51.h) and (ASF/hal/include/hal_wdt.h) with support for volatile inputs
+// This file defines slightly modified copies of functions from (ASF/hri/hri_wdt_d51.h) and (ASF/hal/include/hal_wdt.h). The key difference
+// is that the functions are modified to take in a pointer to a volatile Wdt struct, rather than a pointer to a wdt_descriptor struct or an 
+// arbitrary hardware instance void*. Additionally, the functions have been renamed to be more human-readable.
 
-static inline void watchdog_wait_for_register_sync(volatile Wdt *const watchdog_p, hri_wdt_syncbusy_reg_t reg)
-{
-	while (watchdog_p->SYNCBUSY.reg & reg) {
-	};
+static inline void watchdog_wait_for_register_sync(volatile Wdt *const watchdog_p, hri_wdt_syncbusy_reg_t reg) {
+	while (watchdog_p->SYNCBUSY.reg & reg);
 }
 
-static inline bool watchdog_get_always_on_bit(volatile Wdt *const watchdog_p)
-{
+static inline bool watchdog_get_always_on_bit(volatile Wdt *const watchdog_p) {
 	uint8_t tmp;
 	watchdog_wait_for_register_sync(watchdog_p, WDT_SYNCBUSY_ENABLE | WDT_SYNCBUSY_WEN | WDT_SYNCBUSY_ALWAYSON);
 	tmp = watchdog_p->CTRLA.reg;
@@ -20,20 +19,17 @@ static inline bool watchdog_get_always_on_bit(volatile Wdt *const watchdog_p)
 	return (bool)tmp;
 }
 
-static inline void watchdog_set_early_warning_offset(volatile Wdt *const watchdog_p, hri_wdt_ewctrl_reg_t mask)
-{
+static inline void watchdog_set_early_warning_offset(volatile Wdt *const watchdog_p, hri_wdt_ewctrl_reg_t mask) {
 	WDT_CRITICAL_SECTION_ENTER();
 	watchdog_p->EWCTRL.reg |= WDT_EWCTRL_EWOFFSET(mask);
 	WDT_CRITICAL_SECTION_LEAVE();
 }
 
-static inline void watchdog_enable_early_warning(volatile Wdt *const watchdog_p)
-{
+static inline void watchdog_enable_early_warning(volatile Wdt *const watchdog_p) {
 	watchdog_p->INTENSET.reg = WDT_INTENSET_EW;
 }
 
-static inline void watchdog_set_period(volatile Wdt *const watchdog_p, hri_wdt_config_reg_t data)
-{
+static inline void watchdog_set_period(volatile Wdt *const watchdog_p, hri_wdt_config_reg_t data) {
 	uint8_t tmp;
 	WDT_CRITICAL_SECTION_ENTER();
 	tmp = watchdog_p->CONFIG.reg;
@@ -43,26 +39,22 @@ static inline void watchdog_set_period(volatile Wdt *const watchdog_p, hri_wdt_c
 	WDT_CRITICAL_SECTION_LEAVE();
 }
 
-static inline bool watchdog_get_early_warning_bit(volatile Wdt *const watchdog_p)
-{
+static inline bool watchdog_get_early_warning_bit(volatile Wdt *const watchdog_p) {
 	return (watchdog_p->INTFLAG.reg & WDT_INTFLAG_EW) >> WDT_INTFLAG_EW_Pos;
 }
 
-static inline void watchdog_clear_early_warning_bit(volatile Wdt *const watchdog_p)
-{
+static inline void watchdog_clear_early_warning_bit(volatile Wdt *const watchdog_p) {
 	watchdog_p->INTFLAG.reg = WDT_INTFLAG_EW;
 }
 
-static inline void watchdog_set_clear_register(volatile Wdt *const watchdog_p, hri_wdt_clear_reg_t data)
-{
+static inline void watchdog_set_clear_register(volatile Wdt *const watchdog_p, hri_wdt_clear_reg_t data) {
 	WDT_CRITICAL_SECTION_ENTER();
 	watchdog_p->CLEAR.reg = data;
 	watchdog_wait_for_register_sync(watchdog_p, WDT_SYNCBUSY_CLEAR);
 	WDT_CRITICAL_SECTION_LEAVE();
 }
 
-static inline int32_t watchdog_enable(volatile Wdt *const watchdog_p)
-{
+static inline int32_t watchdog_enable(volatile Wdt *const watchdog_p) {
 	WDT_CRITICAL_SECTION_ENTER();
 	watchdog_p->CTRLA.reg |= WDT_CTRLA_ENABLE;
 	watchdog_wait_for_register_sync(watchdog_p, WDT_SYNCBUSY_ENABLE | WDT_SYNCBUSY_WEN | WDT_SYNCBUSY_ALWAYSON);
@@ -71,8 +63,7 @@ static inline int32_t watchdog_enable(volatile Wdt *const watchdog_p)
 	return ERR_NONE;
 }
 
-static inline int32_t watchdog_disable(volatile Wdt *const watchdog_p)
-{
+static inline int32_t watchdog_disable(volatile Wdt *const watchdog_p) {
 	if (watchdog_get_always_on_bit(watchdog_p)) {
 		return ERR_DENIED;
 	} else {
@@ -85,8 +76,7 @@ static inline int32_t watchdog_disable(volatile Wdt *const watchdog_p)
 	return ERR_NONE;
 }
 
-static inline int32_t watchdog_feed(volatile Wdt *const watchdog_p)
-{
+static inline int32_t watchdog_feed(volatile Wdt *const watchdog_p) {
     watchdog_set_clear_register(watchdog_p, WDT_CLEAR_CLEAR_KEY);
 	return ERR_NONE;
 }
