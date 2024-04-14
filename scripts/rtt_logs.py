efac599af18709c84737f7d5cf9a9cda4d640928
Oren Kohavi <oren_kohavi@brown.edu>
2024-04-14 12:16:26 -0400

    rename rtt logs script

--- scripts/rtt_logs.py
+++ scripts/rtt_logs.py
@@ -1,3 +1,4 @@
+import platform
 import pylink # pip install pylink-square
 
 def open_rtt_channels():
@@ -7,11 +8,18 @@ def open_rtt_channels():
     jlink.rtt_start()
 
     while True:
-        buf1 = jlink.rtt_read(1, 1024)
+        buf1 = jlink.rtt_read(1, 2048)
 
         if buf1:
             text1 = bytes(buf1).decode('ascii', errors='replace')
             print(text1)
 
-if __name__ == "__main__":
+def main():
+    # Sanity check to make sure this is not in WSL
+    if "microsoft" in platform.uname().release:
+        print("This code cannot be executed in a WSL environment. Run natively on Windows instead!")
+        exit(1)
     open_rtt_channels()
+
+if __name__ == "__main__":
+    main()
