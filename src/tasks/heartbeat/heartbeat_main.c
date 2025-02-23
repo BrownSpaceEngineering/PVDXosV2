9e7a0290093f5e65b8abb9146529f60ef62d23d4
SiddhartaL <siddharta_laloux@brown.edu>
2025-02-23 15:27:16 -0500

    heartbeat main refs to get_task() changed to get_current_task()

--- src/tasks/heartbeat/heartbeat_main.c
+++ src/tasks/heartbeat/heartbeat_main.c
@@ -17,16 +17,16 @@ void main_heartbeat(void *pvParameters) {
     info("heartbeat: Task Started!\n");
 
     // Obtain a pointer to the current task within the global task list
-    pvdx_task_t *const current_task = get_task(xTaskGetCurrentTaskHandle());
+    pvdx_task_t *const current_task = get_current_task();
     // Cache the watchdog checkin command to avoid creating it every iteration
     command_t cmd_checkin = get_watchdog_checkin_command(current_task);
 
-    // NOTE: false is on for some reason on the orange LEDs
-    // In release build, make sure orange LEDs are off
-    #if defined(RELEASE)
-        gpio_set_pin_level(LED_Orange1, true);
-        gpio_set_pin_level(LED_Orange2, true);
-    #endif
+// NOTE: false is on for some reason on the orange LEDs
+// In release build, make sure orange LEDs are off
+#if defined(RELEASE)
+    gpio_set_pin_level(LED_Orange1, true);
+    gpio_set_pin_level(LED_Orange2, true);
+#endif
 
     while (true) {
         debug_impl("\n---------- Heartbeat Task Loop ----------\n");
@@ -35,41 +35,41 @@ void main_heartbeat(void *pvParameters) {
         const uint32_t current_time = xTaskGetTickCount();
         debug("heartbeat: Current time is %d\n", current_time);
 
-        // Devbuild heartbeat pattern (Smoothly turning on and off LEDs in a line)
-        #if defined(DEVBUILD)
-            gpio_set_pin_level(LED_Orange1, false);
-            vTaskDelay(pdMS_TO_TICKS(100));
-            gpio_set_pin_level(LED_Orange2, false);
-            vTaskDelay(pdMS_TO_TICKS(100));
-            gpio_set_pin_level(LED_Red, true);
-            vTaskDelay(pdMS_TO_TICKS(400));
-            gpio_set_pin_level(LED_Orange1, true);
-            vTaskDelay(pdMS_TO_TICKS(33));
-            gpio_set_pin_level(LED_Orange2, true);
-            vTaskDelay(pdMS_TO_TICKS(33));
-            gpio_set_pin_level(LED_Red, false);
-            vTaskDelay(pdMS_TO_TICKS(300));
-        #endif
+// Devbuild heartbeat pattern (Smoothly turning on and off LEDs in a line)
+#if defined(DEVBUILD)
+        gpio_set_pin_level(LED_Orange1, false);
+        vTaskDelay(pdMS_TO_TICKS(100));
+        gpio_set_pin_level(LED_Orange2, false);
+        vTaskDelay(pdMS_TO_TICKS(100));
+        gpio_set_pin_level(LED_Red, true);
+        vTaskDelay(pdMS_TO_TICKS(400));
+        gpio_set_pin_level(LED_Orange1, true);
+        vTaskDelay(pdMS_TO_TICKS(33));
+        gpio_set_pin_level(LED_Orange2, true);
+        vTaskDelay(pdMS_TO_TICKS(33));
+        gpio_set_pin_level(LED_Red, false);
+        vTaskDelay(pdMS_TO_TICKS(300));
+#endif
 
-        // Testing heartbeat pattern (Blinking LEDs in an oscillating pattern [ON/OFF/ON] -> [OFF/ON/OFF])
-        #if defined(UNITTEST)
-            gpio_set_pin_level(LED_Orange1, false);
-            gpio_set_pin_level(LED_Orange2, true);
-            gpio_set_pin_level(LED_Red, true);
-            vTaskDelay(pdMS_TO_TICKS(500));
-            gpio_set_pin_level(LED_Orange1, true);
-            gpio_set_pin_level(LED_Orange2, false);
-            gpio_set_pin_level(LED_Red, false);
-            vTaskDelay(pdMS_TO_TICKS(500));
-        #endif
+// Testing heartbeat pattern (Blinking LEDs in an oscillating pattern [ON/OFF/ON] -> [OFF/ON/OFF])
+#if defined(UNITTEST)
+        gpio_set_pin_level(LED_Orange1, false);
+        gpio_set_pin_level(LED_Orange2, true);
+        gpio_set_pin_level(LED_Red, true);
+        vTaskDelay(pdMS_TO_TICKS(500));
+        gpio_set_pin_level(LED_Orange1, true);
+        gpio_set_pin_level(LED_Orange2, false);
+        gpio_set_pin_level(LED_Red, false);
+        vTaskDelay(pdMS_TO_TICKS(500));
+#endif
 
-        // Release heartbeat pattern (Red LED only, blinking at 1Hz)
-        #if defined(RELEASE)
-            gpio_set_pin_level(LED_Red, true);
-            vTaskDelay(pdMS_TO_TICKS(500));
-            gpio_set_pin_level(LED_Red, false);
-            vTaskDelay(pdMS_TO_TICKS(500));
-        #endif
+// Release heartbeat pattern (Red LED only, blinking at 1Hz)
+#if defined(RELEASE)
+        gpio_set_pin_level(LED_Red, true);
+        vTaskDelay(pdMS_TO_TICKS(500));
+        gpio_set_pin_level(LED_Red, false);
+        vTaskDelay(pdMS_TO_TICKS(500));
+#endif
 
         // Check in with the watchdog task
         enqueue_command(&cmd_checkin);
