2341ac1e5d266e7c7b94fe569835c3a33ce317f5
Evan Luo <headlessbear06@gmail.com>
2025-03-06 19:24:20 -0500

    Error1 Debug

--- src/tasks/task_list.c
+++ src/tasks/task_list.c
@@ -116,36 +116,6 @@ pvdx_task_t heartbeat_task = {.name = "Heartbeat",
                               .last_checkin_time_ticks = 0xDEADBEEF,
                               .has_registered = false,
                               .task_type = ACTUATOR};
-pvdx_task_t test_one_task = {.name = "Test 1",
-                             .enabled = true,
-                             .handle = NULL,
-                             .command_queue = NULL,
-                             .init = init_test_one,
-                             .function = main_test_one,
-                             .stack_size = TEST_ONE_TASK_STACK_SIZE,
-                             .stack_buffer = test_one_mem.test_one_task_stack,
-                             .pvParameters = NULL,
-                             .priority = 1,
-                             .task_tcb = &test_one_mem.test_one_task_tcb,
-                             .watchdog_timeout_ms = 10000,
-                             .last_checkin_time_ticks = 0xDEADBEEF,
-                             .has_registered = false,
-                             .task_type = TESTING};
-pvdx_task_t test_two_task = {.name = "Test 2",
-                             .enabled = true,
-                             .handle = NULL,
-                             .command_queue = NULL,
-                             .init = init_test_two,
-                             .function = main_test_two,
-                             .stack_size = TEST_ONE_TASK_STACK_SIZE,
-                             .stack_buffer = test_two_mem.test_two_task_stack,
-                             .pvParameters = NULL,
-                             .priority = 1,
-                             .task_tcb = &test_two_mem.test_two_task_tcb,
-                             .watchdog_timeout_ms = 10000,
-                             .last_checkin_time_ticks = 0xDEADBEEF,
-                             .has_registered = false,
-                             .task_type = TESTING};
 
 // and define their constant pointers
 pvdx_task_t *const p_watchdog_task = &watchdog_task;
@@ -155,8 +125,6 @@ pvdx_task_t *const p_magnetometer_task = &magnetometer_task;
 pvdx_task_t *const p_shell_task = &shell_task;
 pvdx_task_t *const p_display_task = &display_task;
 pvdx_task_t *const p_heartbeat_task = &heartbeat_task;
-pvdx_task_t *const p_test_one_task = &test_one_task;
-pvdx_task_t *const p_test_two_task = &test_two_task;
 pvdx_task_t *const task_list_null_terminator = NULL;
 
 // Global list of all tasks running on PVDXos (see `pvdx_task_t` definition in globals.h)
@@ -166,8 +134,8 @@ pvdx_task_t *const task_list_null_terminator = NULL;
 // NOTE: Watchdog task must be first in the list, Command Dispatcher second, and Task Manager third.
 // If you change the order of any of these, make sure that main.c reflects the change and update this comment.
 pvdx_task_t *task_list[] = {
-    p_watchdog_task, p_command_dispatcher_task, p_task_manager_task, p_magnetometer_task, p_shell_task,
-    p_display_task,  p_heartbeat_task,          p_test_one_task,     p_test_two_task,     task_list_null_terminator,
+    p_watchdog_task, p_command_dispatcher_task, p_task_manager_task,       p_magnetometer_task, p_shell_task,
+    p_display_task,  p_heartbeat_task,          task_list_null_terminator,
 };
 
 /**
@@ -186,8 +154,4 @@ inline pvdx_task_t *get_current_task(void) {
 // a command from the task's command queue.
 inline TickType_t get_command_queue_block_time_ticks(pvdx_task_t *const task) {
     return pdMS_TO_TICKS(task->watchdog_timeout_ms / 2);
-}
-
-inline bool should_checkin(pvdx_task_t *const task) {
-    return xTaskGetTickCount() - task->last_checkin_time_ticks > pdMS_TO_TICKS(task->watchdog_timeout_ms / 3);
 }
