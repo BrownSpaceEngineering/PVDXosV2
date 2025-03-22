b26f48e9bae6cd863ed638af046f6d3ebed8aa3a
sjuknelis <sjuknelis24@nobles.edu>
2025-03-21 23:29:20 -0400

    feat: mram validation algorithm

--- src/tasks/mram/mram_driver.c
+++ src/tasks/mram/mram_driver.c
@@ -6,14 +6,38 @@
 
 #define massert(x) ((x) ? 0 : fatal("massert failed"));
 
-#define MRAM_MAX_ADDRESS    (1 << 24)
+#define MRAM_MAX_ADDRESS        8388608 // 8MiB
+#define MRAM_REGION_SIZE        1000000
+#define MRAM_REGION_BUF_SIZE    4096
 
-#define MRAM_CS_LOW()       gpio_set_pin_level(0, 0)
-#define MRAM_CS_HIGH()      gpio_set_pin_level(0, 1)
+#define MRAM_CS_LOW()           gpio_set_pin_level(0, 0)
+#define MRAM_CS_HIGH()          gpio_set_pin_level(0, 1)
 
-#define MRAM_CMD_READ       0x03
-#define MRAM_CMD_WRITE_EN   0x06
-#define MRAM_CMD_WRITE      0x02
+#define MRAM_CMD_READ           0x03
+#define MRAM_CMD_WRITE_EN       0x06
+#define MRAM_CMD_WRITE          0x02
+
+void mram_write_raw(long pos, long len, const char *buf) {
+    massert(0 <= pos && pos < MRAM_MAX_ADDRESS);
+    massert(0 <= len && pos + len < MRAM_MAX_ADDRESS);
+
+    // Send write enable command, otherwise write command will be ignored
+    MRAM_CS_LOW();
+    char tx_buf[] = {MRAM_CMD_WRITE_EN};
+    massert(SPI_0.io.write(&SPI_0, tx_buf, 1) == 1);
+    MRAM_CS_HIGH();
+
+    MRAM_CS_LOW();
+
+    // Send write command
+    tx_buf[0] = MRAM_CMD_WRITE;
+    massert(SPI_0.io.write(&SPI_0, tx_buf, 1) == 1);
+
+    // Send len bytes of data to be written
+    massert(SPI_0.io.write(&SPI_0, buf, len));
+
+    MRAM_CS_HIGH();
+}
 
 void mram_read_raw(long pos, long len, char *buf) {
     massert(0 <= pos && pos < MRAM_MAX_ADDRESS);
@@ -36,24 +60,66 @@ void mram_read_raw(long pos, long len, char *buf) {
     MRAM_CS_HIGH();
 }
 
-void mram_write_raw(long pos, long len, const char *buf) {
-    massert(0 <= pos && pos < MRAM_MAX_ADDRESS);
-    massert(0 <= len && pos + len < MRAM_MAX_ADDRESS);
+void mram_write(long pos, long len, const char *buf) {
+    massert(pos < MRAM_REGION_SIZE);
+    massert(pos + len < MRAM_REGION_SIZE);
 
-    // Send write enable command, otherwise write command will be ignored
-    MRAM_CS_LOW();
-    char tx_buf[] = {MRAM_CMD_WRITE_EN};
-    massert(SPI_0.io.write(&SPI_0, tx_buf, 1) == 1);
-    MRAM_CS_HIGH();
+    // Write a copy of buf to each region at index pos
+    mram_write_raw(pos, len, buf);
+    mram_write_raw(pos + MRAM_REGION_SIZE, len, buf);
+    mram_write_raw(pos + MRAM_REGION_SIZE * 2, len, buf);
+}
 
-    MRAM_CS_LOW();
+void mram_read(long pos, long len, char *buf) {
+    massert(pos < MRAM_REGION_SIZE);
+    massert(pos + len < MRAM_REGION_SIZE);
 
-    // Send write command
-    tx_buf[0] = MRAM_CMD_WRITE;
-    massert(SPI_0.io.write(&SPI_0, tx_buf, 1) == 1);
+    // Read len bytes from each region starting at index pos
+    char r1_buf[MRAM_REGION_BUF_SIZE];
+    mram_read_raw(pos, len, r1_buf);
+    char r2_buf[MRAM_REGION_BUF_SIZE];
+    mram_read_raw(pos + MRAM_REGION_SIZE, len, r2_buf);
+    char r3_buf[MRAM_REGION_BUF_SIZE];
+    mram_read_raw(pos + MRAM_REGION_SIZE * 2, len, r3_buf);
 
-    // Send len bytes of data to be written
-    massert(SPI_0.io.write(&SPI_0, buf, len));
+    // Validate the data, which we expect to have been the same in each region
+    for (long i = 0; i < len; i++) {
+        char valid;
 
-    MRAM_CS_HIGH();
+        if (r1_buf[i] == r2_buf[i] && r1_buf[i] == r3_buf[i]) {
+            // This byte was the same in each region
+            valid = r1_buf[i];
+        } else {
+            // This byte was different in at least one region, so we compare it
+            // across regions bit-by-bit
+            int corrections = 8;
+            valid = 0;
+            for (int j = 0; j < 8; j++) {
+                // Get the jth bit of this byte in each region
+                char r1_bit = r1_buf[i] & (1 << j);
+                char r2_bit = r2_buf[i] & (1 << j);
+                char r3_bit = r3_buf[i] & (1 << j);
+
+                // Set this bit in valid to 1 if it is 1 in two regions
+                if ((r1_bit & r2_bit) || (r1_bit & r3_bit) || (r2_bit & r3_bit)) {
+                    valid |= (1 << j);
+                }
+
+                // If this bit was the same in every region, this was not an
+                // actual correction
+                if (r1_bit == r2_bit && r1_bit == r3_bit) {
+                    corrections--;
+                }
+            }
+            warning("mram: byte verification failure resolved by %d bit corrections", corrections);
+
+            // Rewrite the corrected byte to all regions
+            mram_write(pos + i, 1, &valid);
+        }
+
+        // Put the validated byte in the return buffer
+        if (buf != NULL) {
+            buf[i] = valid;
+        }
+    }
 }
