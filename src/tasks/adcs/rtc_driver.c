edc740f0704f09f98955ca41cdc741010bf6f322
AlexanderThaep <odomsathshia_tep@brown.edu>
2025-11-09 17:43:37 -0500

    fix raw register read of rtc hardware device

--- src/tasks/adcs/rtc_driver.c
+++ src/tasks/adcs/rtc_driver.c
@@ -17,12 +17,15 @@
  * \returns status_t SUCCESS if initialization was successful
  */
 status_t init_rtc_hardware(void) {
-    timer_init(&TIMER_0);
     timer_start(&TIMER_0);
 
-    uint32_t c1 = hri_rtcmode0_get_COUNT_reg(&TIMER_0.device, 4294967295UL);
+    uint32_t c1 = hri_rtcmode0_get_COUNT_reg((&TIMER_0.device)->hw, 4294967295UL);
     delay_ms(1000);
-    uint32_t c2 = hri_rtcmode0_get_COUNT_reg(&TIMER_0.device, 4294967295UL);
+    uint32_t c2 = hri_rtcmode0_get_COUNT_reg((&TIMER_0.device)->hw, 4294967295UL);
+
+
+    info("c1 rtc counter: \n", c1);
+    info("c2 rtc counter: \n", c2);
     
     return SUCCESS;
 }
