67404bb138868ecfef1162b7dc974ed45c9a3a4e
AlexanderThaep <odomsathshia_tep@brown.edu>
2025-11-09 17:35:35 -0500

    add doc comments

--- src/tasks/adcs/rtc_driver.c
+++ src/tasks/adcs/rtc_driver.c
@@ -1,11 +1,28 @@
+/**
+ * rtc_driver.c
+ *
+ * Hardware driver for RTC timer used in ADCS algorithms
+ *
+ * Created: November 9, 2025
+ * Authors: Alexander Thaep
+ */
 #include "adcs_task.h"
 #include "rtc_driver.h"
 
-void init_rtc_hardware(void) {
+/**
+ * \fn init_rtc_hardware
+ *
+ * \brief Initialize RTC timer hardware
+ *
+ * \returns status_t SUCCESS if initialization was successful
+ */
+status_t init_rtc_hardware(void) {
     timer_init(&TIMER_0);
     timer_start(&TIMER_0);
 
     uint32_t c1 = hri_rtcmode0_get_COUNT_reg(&TIMER_0.device, 4294967295UL);
     delay_ms(1000);
     uint32_t c2 = hri_rtcmode0_get_COUNT_reg(&TIMER_0.device, 4294967295UL);
+    
+    return SUCCESS;
 }
