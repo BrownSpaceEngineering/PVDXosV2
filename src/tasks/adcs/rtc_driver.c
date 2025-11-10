38149f490457b24e67d05cb496da8fd73e572aff
AlexanderThaep <odomsathshia_tep@brown.edu>
2025-11-09 22:16:47 -0500

    implemented get_rtc_count and silenced instrinsics.h

--- src/tasks/adcs/rtc_driver.c
+++ src/tasks/adcs/rtc_driver.c
@@ -9,6 +9,9 @@
 #include "adcs_task.h"
 #include "rtc_driver.h"
 
+static const void *rtc_hw;
+static uint32_t rtc_count;
+
 /**
  * \fn init_rtc_hardware
  *
@@ -17,15 +20,27 @@
  * \returns status_t SUCCESS if initialization was successful
  */
 status_t init_rtc_hardware(void) {
-    timer_start(&TIMER_0);
+    rtc_hw = (&TIMER_0.device)->hw;
+	hri_rtcmode0_clear_CTRLA_ENABLE_bit(rtc_hw);
+	hri_rtcmode0_write_COUNT_reg(rtc_hw, 0);
+	hri_rtcmode0_wait_for_sync(rtc_hw, RTC_MODE0_SYNCBUSY_COUNT);
+	hri_rtcmode0_set_CTRLA_ENABLE_bit(rtc_hw);
 
-    uint32_t c1 = hri_rtcmode0_get_COUNT_reg((&TIMER_0.device)->hw, 4294967295UL);
-    delay_ms(1000);
-    uint32_t c2 = hri_rtcmode0_get_COUNT_reg((&TIMER_0.device)->hw, 4294967295UL);
-
-
-    info("c1 rtc counter: \n", c1);
-    info("c2 rtc counter: \n", c2);
-    
     return SUCCESS;
+}
+
+/**
+ * \fn get_rtc_count
+ *
+ * \brief Get RTC timer hardware counter
+ *
+ * \returns uint32_t of the counter register belonging to RTC mode 0
+ */
+uint32_t get_rtc_count(void) {
+    if (!rtc_hw) {
+        warning("Attempting to get RTC count before initializing RTC");
+        return 0;
+     }
+    rtc_count = hri_rtcmode0_get_COUNT_reg(rtc_hw, 4294967295UL);
+    return rtc_count;
 }
