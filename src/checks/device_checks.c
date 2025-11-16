376f6bda834a8762c569f63f7467648f1cf95f9d
Zach Mahan <zach.mahan.zm@gmail.com>
2025-11-16 14:14:12 -0500

    Define basic a device check and initialization interface; add a device_id_t enum to track device states (#25)
    * add device_ide_e, add a device checks array for device state
    
    * finish basic device checks api and clean up code
    
    * make device_check_state_t internal to device_checks.c since it is an implementation detail
    
    * Fix Makefile to work correctly with bootloader (#21)
    
    * fix: connect command stops at main for ease of debugging, use connect_bl to stop at bootloader
    
    * merged simon's gdbcmd fix
    
    ---------
    
    Co-authored-by: Aanya Agrawal <purplishwings@gmail.com>
    
    * fixed readme to contain updated instructions
    
    * config: Overwrite pin mappings so that devices operate on separate SERCOMs
    
    * fix: Inner makefile already includes 'update_asf' build target
    
    * revert: Previous two commits
    
    * config: (using inner update_asf target) Overwrite pin mappings so that devices operate on separate SERCOMs
    
    * fix: Update hardcoded 'SPI_0' and 'I2C_0' descriptors with dedicated SERCOM descriptors for display and magnetometer
    
    * add references for checks to makefile
    
    * remove unnecessary include
    
    * _t
    
    * resolve TODO
    
    ---------
    
    Co-authored-by: Simon Juknelis <81927579+sjuknelis@users.noreply.github.com>
    Co-authored-by: Aanya Agrawal <purplishwings@gmail.com>
    Co-authored-by: Tanish Makadia <tanishmakadia@gmail.com>

---
+++ src/checks/device_checks.c
@@ -0,0 +1,70 @@
+/**
+ * src/checks/device_checks.c
+ *
+ * header file for checking the state of hardware device for PVDX
+ *
+ * Created: 20251102 SUN
+ * Updated: 20251112 TUE
+ * Authors: Zach Mahan
+ */
+
+#include "checks/device_checks.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "globals.h"
+#include "logging.h"
+
+// ~~~ for safety, these structures should be local to this source file ~~~
+typedef struct device_check_state {
+    bool checked;
+    bool valid;
+} device_check_state_t;
+
+static device_check_state_t device_states[NUM_DEVICES] = {[0 ... NUM_DEVICES - 1] = {.checked = false, .valid = false}};
+
+static bool (*device_check_functions[NUM_DEVICES])(void) = {
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+};
+
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+bool check_all_devices_on_startup(void) {
+    bool at_least_one_failed = false;
+
+    for (device_id_t device_id = 0; device_id < NUM_DEVICES; device_id++) {
+        at_least_one_failed = at_least_one_failed || !check_device(device_id);
+    }
+
+    return at_least_one_failed;
+}
+
+bool check_device(device_id_t device_id) {
+    // guard against invalid device_id given
+    if (device_id >= NUM_DEVICES) {
+        fatal("[ERROR] Invalid device_id_e passed to `check_devices`");
+        return false;
+    }
+    // guard against calling a null fn
+    if (device_check_functions[device_id] == NULL) {
+        fatal("[ERROR] check function for device_id: %p not defined", device_check_functions[device_id]);
+        return false;
+    }
+    // only run the check function if "checked" is false for a device
+    if (!device_states[device_id].checked) {
+        device_states[device_id].valid = device_check_functions[device_id]();
+        device_states[device_id].checked = true;
+    }
+    return device_states[device_id].valid;
+}
+
+void uncheck_device(device_id_t device_id) {
+    device_states[device_id].checked = false;
+}
+
+bool check_and_uncheck_device(device_id_t device_id) {
+    bool original_checked = device_states[device_id].checked;
+    device_states[device_id].checked = false;
+    return original_checked;
+}
