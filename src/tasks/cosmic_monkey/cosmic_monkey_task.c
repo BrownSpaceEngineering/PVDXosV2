f7dcad186308f413e78c9d7572839a27fbb98c14
Aanya Agrawal <purplishwings@gmail.com>
2025-11-20 20:24:53 -0500

    cosmic monkey onboarding task

--- src/tasks/cosmic_monkey/cosmic_monkey_task.c
+++ src/tasks/cosmic_monkey/cosmic_monkey_task.c
@@ -33,20 +33,27 @@ static const int EIGHTEEN_BIT_MASK = 0x3FFFF;
  * \warning only used for testing; this function should never be called!
  */
 status_t perform_flip() {
-    /* Generate random number */
-    uint32_t rand_int = rand_sync_read32(&RAND_0); // Mask the first 21 bits
-    uintptr_t p_memory_addr =
-        VALID_MEMORY_RANGE_START + (rand_int & EIGHTEEN_BIT_MASK); // Isolate 18 bits of randomness to pick a random memory address
-    int bit_position = (rand_int & RAND_THREE_BIT_MASK) >> VALID_MEMORY_RANGE_IN_BYTES; // Pick the next 3 bits as the index
-    if (8 <= bit_position) {
-        return ERROR_SANITY_CHECK_FAILED;
-    }
-    uint8_t byte_flip_mask = 1 << bit_position; // Generates the a mask of the form 0 ... 010 ... 0
+    // Implement Cosmic Monkey here! 
+    // You should only change bits at or after VALID_MEMORY_RANGE_START
+    // You can get a random number using the following line:
+    uint32_t rand_int = rand_sync_read32(&RAND_0); // Provides a 21-bit random number
 
-    char* addr = (char*)p_memory_addr; // Gets pointer to the byte that will be modified
+    // Now, using your Cosmic Monkey experience, flip a bit at a random position starting 
+    // at VALID_MEMORY_RANGE_START and within a range of (2^18-1)
+    
+    // Hint: you have 21 bits of randomness to work with. 
+    // How many bits do you need to randomize the bit position in a byte?
+    // How many bits do you need to randomize the index of the address after VALID_MEMORY_RANGE_START?
 
-    *addr = *addr ^ byte_flip_mask; // Apply the byte_mask with an XOR to the selected byte
-
+    // uintptr_t p_memory_addr = ??
+    // continue ...
+    
     warning("cosmic_monkey: Flipped bit at address 0x%08x\n", p_memory_addr);
     return SUCCESS;
+
+
+    // Once you're done...
+    // 1. Look at the cosmic_monkey_main.c and cosmic_monkey_task.h files. Do you understand what's happening here? Clarify any confusion with FSW!
+    // 2. Figure out how the Cosmic Monkey task is scheduled. Where do we call it from? Where do we set the 'frequency'?
+    // 3. Set the devbuild frequency to 20 and watch the OS corrupt and reboot itself repeatedly...
 }
