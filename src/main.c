fd5447ee9af6e999ea330662204a2cf250c599b0
sjuknelis <simonas_juknelis@brown.edu>
2026-02-25 20:02:36 -0500

    add reset cause message

--- src/main.c
+++ src/main.c
@@ -13,24 +13,52 @@
 #include "mram.h"
 #include "reflash.h"
 
+#define RSTC_RCAUSE (0x40000C00UL)  // Reset Cause Register
+
 cosmic_monkey_task_arguments_t cm_args = {0};
 
-static status_t PVDX_init(void) {
+void init_logging(void) {
     // Segger Buffer 0 is pre-configured at compile time according to segger documentation
     // Config the logging output channel (assuming it's not zero)
     if (LOGGING_RTT_OUTPUT_CHANNEL != 0) {
         SEGGER_RTT_ConfigUpBuffer(LOGGING_RTT_OUTPUT_CHANNEL, "Log Output", SEGGER_RTT_LOG_BUFFER, SEGGER_RTT_LOG_BUFFER_SIZE,
                                   SEGGER_RTT_MODE_NO_BLOCK_SKIP);
     }
-    return SUCCESS;
+}
+
+void report_reset_cause(void) {
+    uint8_t cause = *((uint8_t *)RSTC_RCAUSE);
+
+    info_impl("[+] Last reset cause(s):\n");
+    if (cause & 0x01) {
+        info_impl("- Power on Reset\n");
+    }
+    if (cause & 0x02) {
+        info_impl("- Brown-Out Detector Core Reset\n");
+    }
+    if (cause & 0x04) {
+        info_impl("- Brown-Out Detector VDD Reset\n");
+    }
+    if (cause & 0x10) {
+        info_impl("- NVM Reset\n");
+    }
+    if (cause & 0x20) {
+        info_impl("- External Reset\n");
+    }
+    if (cause & 0x40) {
+        info_impl("- Watchdog Reset\n");
+    }
+    if (cause & 0x80) {
+        info_impl("- System Reset Request\n");
+    }
 }
 
 int main(void) {
-    /* ---------- HARDWARE & LOGGING INITIALIZATION + BOOTLOADER CHECK ---------- */
+    /* ---------- HARDWARE & LOGGING INITIALIZATION ---------- */
 
     /* Initializes MCU, drivers and middleware */
     atmel_start_init();
-    PVDX_init();
+    init_logging();
     // info_impl(RTT_CTRL_RESET RTT_CTRL_CLEAR); // Reset the terminal
     info_impl("--- Atmel & Hardware Initialization Complete ---\n");
     info_impl("[+] Build Type: %s\n", BUILD_TYPE);
@@ -38,6 +66,7 @@ int main(void) {
     info_impl("[+] Build Time: %s\n", BUILD_TIME);
     info_impl("[+] Built from branch: %s\n", GIT_BRANCH_NAME);
     info_impl("[+] Built from commit: %s\n", GIT_COMMIT_HASH);
+    report_reset_cause();
 
 #ifdef MRAM_OS_READ
     reflash_bootloaders();
